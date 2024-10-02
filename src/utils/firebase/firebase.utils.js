814338776162f3b544abce372a8817307cdebfa2
GSingh-dev <2gurwinderjit2003@gmail.com>
2024-10-01 22:01:48 -0700

    Update firebase.utils.js

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,9 +3,9 @@ import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from "
 import {getFirestore, doc, getDoc, setDoc} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2BCluW4c1Gz0Img3Z1_1aHXI4NrCj9rk",
-  authDomain: "ecommerce-db-5651e.firebaseapp.com",
-  projectId: "ecommerce-db-5651e",
+  apiKey: "xxxx",
+  authDomain: "ecommerce-db.firebaseapp.com",
+  projectId: "ecommerce-db-",
   storageBucket: "ecommerce-db-5651e.appspot.com",
   messagingSenderId: "202862778729",
   appId: "1:202862778729:web:c535595ff3c1df756c45f8"
